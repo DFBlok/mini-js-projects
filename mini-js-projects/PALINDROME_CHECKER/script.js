c7b4cbd714e035df648c8f57064b3fc3b9fecb37
DFBlok <s219392862@mandela.ac.za>
2024-03-11 12:43:35 +0200

    palindrome completed

--- mini-js-projects/PALINDROME_CHECKER/script.js
+++ mini-js-projects/PALINDROME_CHECKER/script.js
@@ -1,1 +1,21 @@
-alert("Hello Woe");
+const input = document.getElementById("input");
+
+/* function reverseString(str) {
+  return str.split("").reverse().joint("");
+} */
+
+function check() {
+    /* console.log(input.value) */
+  const value = input.value;
+  const reverse = value.split("").reverse().join("");
+
+  if (value === reverse) {
+/*     alert("Congrats your word is PALINDROME"); */
+    document.getElementById("render").textContent = "Congrats your word is PALINDROME";
+  } else {
+/*     alert("Word is NOT a PALINDROME"); */
+    document.getElementById("render").textContent = "Word is NOT a PALINDROME";
+  }
+
+  input.value = "";
+}
